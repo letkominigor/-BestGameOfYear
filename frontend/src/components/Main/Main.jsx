66b6651f96a47ed43fcd64fa7fa2a3bc68404660
Pavel <gomer_74@mail.ru>
2022-07-01 15:18:06 +0300

    commit

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -5,21 +5,7 @@ import './Main.css'
 function Main() {
   return (
   <>
-  {/* <Question /> */}
-  <div className="flipper" id="target">
-
-    <div className="front">
-      {/* <a href="#target" className="home-button">START</a> */}
-    </div>
-
-    <div className="back">
-      {/* <a href="#close" className="close-button">RESTART</a> */}
-        <div className="inner">
-          <Table/>
-        </div>
-    </div>
-
-  </div>
+  <h1>Даров</h1>
   </>
   );
 }
