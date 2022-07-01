be26e386223e222da3a23e6bda6b623420eab11a
Андрей Волков <nitghwolf13@gmail.com>
2022-07-01 11:13:17 +0300

    add registration

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -2,15 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import NavBar from '../NavBar/NavBar';
 import Main from '../Main/Main';
+import Registration from '../Registration/Registration';
+import store from '../../store';
 
 function App() {
   return (
-    <>
-      <NavBar />
+    <Provider store={store}>
+    <NavBar />
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/registration' element={<Registration />} />
       </Routes>
-    </>
+    </Provider>
   );
 }
 
